5dcf38cbf2c1acec4d48bcf67314eec9dca58d34
Xiaoxue Wang <xiaoxwan@redhat.com>
2025-08-05 15:49:52 +0800

    fix: container spec exec with env and cmd precheck
     - exec with env path PATH_ENV_OVERRIDER, derived from SAFE_ENV
     - pre-check on the existence of cmds in container exec
     - customize the relative_path for container type providers
    
    Signed-off-by: Xiaoxue Wang <xiaoxwan@redhat.com>

--- insights/tests/datasources/container/test_nginx_conf.py
+++ insights/tests/datasources/container/test_nginx_conf.py
@@ -22,46 +22,47 @@ find_list_ng = [
     '/opt/nginx/nginx.conf.d/ss-nginx.conf',
 ]
 
+CONTAINER_CMD = '%s exec -e "PATH=$PATH" %s bash -c "command -v find && find /etc/ /opt/ *.conf"'
+
 
 @patch("insights.core.spec_factory.CommandOutputProvider.validate", return_value=None)
 @patch("insights.core.spec_factory.CommandOutputProvider.load", return_value=None)
-@patch("insights.core.spec_factory.CommandOutputProvider.content",
-        new_callable=PropertyMock, return_value=find_list)
+@patch(
+    "insights.core.spec_factory.CommandOutputProvider.content",
+    new_callable=PropertyMock,
+    return_value=find_list,
+)
 def test_nginx_conf(validate, load, find_l):
     find_nginx_confs = [
-        ContainerCommandProvider(
-            'podman exec 03e2861336a7 find /etc/ /opt/ *.conf',
-            None, 'rhel8_nginx'),
-        ContainerCommandProvider(
-            'docker exec 03e2861336a8 find /etc/ /opt/ *.conf',
-            None, 'rhel7_nginx'),
+        ContainerCommandProvider(CONTAINER_CMD % ("podman", "03e2861336a7"), None, 'rhel8_nginx'),
+        ContainerCommandProvider(CONTAINER_CMD % ("docker", "03e2861336a8"), None, 'rhel7_nginx'),
     ]
-    broker = {
-        LocalSpecs.container_find_etc_opt_conf: find_nginx_confs,
-        HostContext: None}
+    broker = {LocalSpecs.container_find_etc_opt_conf: find_nginx_confs, HostContext: None}
 
     ret = nginx_conf(broker)
     assert len(ret) == 8
     assert ('rhel8_nginx', 'podman', '03e2861336a7', '/etc/nginx/nginx.conf') in ret
-    assert ('rhel7_nginx', 'docker', '03e2861336a8', '/opt/rh-nginx/nginx.conf.d/ss-nginx.conf') in ret
+    assert (
+        'rhel7_nginx',
+        'docker',
+        '03e2861336a8',
+        '/opt/rh-nginx/nginx.conf.d/ss-nginx.conf',
+    ) in ret
 
 
 @patch("insights.core.spec_factory.CommandOutputProvider.validate", return_value=None)
 @patch("insights.core.spec_factory.CommandOutputProvider.load", return_value=None)
-@patch("insights.core.spec_factory.CommandOutputProvider.content",
-        new_callable=PropertyMock, return_value=find_list_ng)
+@patch(
+    "insights.core.spec_factory.CommandOutputProvider.content",
+    new_callable=PropertyMock,
+    return_value=find_list_ng,
+)
 def test_nginx_conf_empty(validate, load, find_l):
     find_nginx_confs_ng = [
-        ContainerCommandProvider(
-            'podman exec 03e2861336a7 find /etc/ /opt/ *.conf',
-            None, 'rhel8_nginx'),
-        ContainerCommandProvider(
-            'docker exec 03e2861336a8 find /etc/ /opt/ *.conf',
-            None, 'rhel7_nginx'),
+        ContainerCommandProvider(CONTAINER_CMD % ("podman", "03e2861336a7"), None, 'rhel8_nginx'),
+        ContainerCommandProvider(CONTAINER_CMD % ("docker", "03e2861336a8"), None, 'rhel7_nginx'),
     ]
-    broker = {
-        LocalSpecs.container_find_etc_opt_conf: find_nginx_confs_ng,
-        HostContext: None}
+    broker = {LocalSpecs.container_find_etc_opt_conf: find_nginx_confs_ng, HostContext: None}
 
     with pytest.raises(SkipComponent):
         ret = nginx_conf(broker)
